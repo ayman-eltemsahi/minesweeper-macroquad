95058450edb4ec3fa21c642973fa3accd09b66e0
Ayman Eltemsahi <aymaneltemsahi@hotmail.com>
2024-05-24 10:35:08 +0200

    refactoring of vector2

--- src/vector2.rs
+++ src/vector2.rs
@@ -6,61 +6,75 @@ pub struct Vector2<T> {
     pub y: T,
 }
 
-impl<T> Vector2<T>
-where
-    T: Add<T, Output = T> + Copy,
-    T: Sub<T, Output = T>,
-    T: Mul<T, Output = T>,
-    T: Div<T, Output = T>,
-    T: std::cmp::PartialOrd<T>,
-{
-    pub const fn new(x: T, y: T) -> Vector2<T> {
+impl<T> Vector2<T> {
+    pub const fn new(x: T, y: T) -> Self {
         Vector2 { x, y }
     }
 
-    pub fn add(&self, other: Vector2<T>) -> Vector2<T> {
+    pub fn add(&self, other: Vector2<T>) -> Self
+    where
+        T: Add<T, Output = T> + Copy,
+    {
         Vector2 {
             x: self.x + other.x,
             y: self.y + other.y,
         }
     }
 
-    pub fn add_val(&self, val: T) -> Vector2<T> {
+    pub fn add_val(&self, val: T) -> Self
+    where
+        T: Add<T, Output = T> + Copy,
+    {
         Vector2 {
             x: self.x + val,
             y: self.y + val,
         }
     }
 
-    pub fn sub(&self, other: Vector2<T>) -> Vector2<T> {
+    pub fn sub(&self, other: Vector2<T>) -> Self
+    where
+        T: Sub<T, Output = T> + Copy,
+    {
         Vector2 {
             x: self.x - other.x,
             y: self.y - other.y,
         }
     }
 
-    pub fn scale(&self, val: T) -> Vector2<T> {
+    pub fn scale(&self, val: T) -> Self
+    where
+        T: Mul<T, Output = T> + Copy,
+    {
         Vector2 {
             x: self.x * val,
             y: self.y * val,
         }
     }
 
-    pub fn div(&self, other: Vector2<T>) -> Vector2<T> {
+    pub fn div(&self, other: Vector2<T>) -> Self
+    where
+        T: Div<T, Output = T> + Copy,
+    {
         Vector2 {
             x: self.x / other.x,
             y: self.y / other.y,
         }
     }
 
-    pub fn flip(&self) -> Vector2<T> {
+    pub fn flip(&self) -> Self
+    where
+        T: Copy,
+    {
         Vector2 {
             x: self.y,
             y: self.x,
         }
     }
 
-    pub fn min_component(&self) -> T {
+    pub fn min_component(&self) -> T
+    where
+        T: PartialOrd + Copy,
+    {
         if self.x < self.y {
             self.x
         } else {
@@ -70,7 +84,7 @@ where
 }
 
 impl From<Vector2<i32>> for Vector2<f32> {
-    fn from(v: Vector2<i32>) -> Vector2<f32> {
+    fn from(v: Vector2<i32>) -> Self {
         Vector2 {
             x: v.x as f32,
             y: v.y as f32,
